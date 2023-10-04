7ef00e646227553dd7be4c8e36a4bf6c73755df7
G Mahendran <mahe7400@gmail.com>
2023-10-04 09:56:29 +0530

    numpy workouts

---
+++ array slice.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+
+print(arr[:4])
+
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+
+print(arr[-3:-1])
+
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+
+print(arr[1:5:2])
---
+++ numpy dim.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+arr = np.array(42)
+
+print(arr)
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+
+print(arr)
+
+
+import numpy as np
+
+arr = np.array([[1, 2, 3], [4, 5, 6]])
+
+print(arr)
